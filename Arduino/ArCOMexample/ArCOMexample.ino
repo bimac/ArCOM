6552aee9e854ba95d6dcd2989b81c2c86861e5c7
Florian Rau <bimac@users.noreply.github.com>
2022-04-04 06:26:19 +0200

    Update ArCOMexample.ino

--- Arduino/ArCOMexample/ArCOMexample.ino
+++ Arduino/ArCOMexample/ArCOMexample.ino
@@ -1,22 +1,17 @@
 /*
-----------------------------------------------------------------------------
-
-This file is part of the Sanworks ArCOM repository
+ArCOM
 Copyright (C) 2016 Sanworks LLC, Sound Beach, New York, USA
-
-----------------------------------------------------------------------------
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, version 3.
 
-This program is distributed  WITHOUT ANY WARRANTY and without even the 
-implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  
+This program is distributed  WITHOUT ANY WARRANTY and without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
 See the GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 */
 
 /* This example code receives 100 16-bit signed integers and 100 32-bit unsigned integers, modifies them, and returns them to MATLAB.
@@ -34,22 +29,28 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include "ArCOM.h"
 ArCOM myUSB(SerialUSB); // Sets ArCOM to wrap SerialUSB (could also be Serial, Serial1, etc.)
+ArCOM Serial1COM(Serial1);        // Wrap Serial1 (UART on Arduino M0, Due + Teensy 3.X)
 short waveform[100] = {0};
 unsigned long timestamps[100] = {0};
 
 void setup() {
+  Serial1.begin(1312500);    
   SerialUSB.begin(115200);
 }
 
 void loop() {
-  if (myUSB.available()) { // Wait for MATLAB to send data
-    myUSB.readInt16Array(waveform, 100); // Read 100 16-bit signed integers (e.g. a waveform)
-    myUSB.readUint32Array(timestamps, 100); // Read 100 32-bit unsigned integers (e.g. timing data)
-    for (int i = 0; i < 100; i++) {
-      waveform[i] = waveform[i]-1; // Modify each waveform sample
-      timestamps[i] = timestamps[i]+1; // Modify each timestamp
-    }
-    myUSB.writeInt16Array(waveform, 100); // Write 100 16-bit signed integers
-    myUSB.writeUint32Array(timestamps, 100); // Write 100 32-bit unsigned integers (e.g. timing data)
+  if (myUSB.available()) {
+    myUSB.readUint8();
+    myUSB.writeDouble(M_PI);
   }
+  // if (myUSB.available()) { // Wait for MATLAB to send data
+  //   myUSB.readInt16Array(waveform, 100); // Read 100 16-bit signed integers (e.g. a waveform)
+  //   myUSB.readUint32Array(timestamps, 100); // Read 100 32-bit unsigned integers (e.g. timing data)
+  //   for (int i = 0; i < 100; i++) {
+  //     waveform[i] = waveform[i]-1; // Modify each waveform sample
+  //     timestamps[i] = timestamps[i]+1; // Modify each timestamp
+  //   }
+  //   myUSB.writeInt16Array(waveform, 100); // Write 100 16-bit signed integers
+  //   myUSB.writeUint32Array(timestamps, 100); // Write 100 32-bit unsigned integers (e.g. timing data)
+  // }
 }
